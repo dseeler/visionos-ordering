bde986b715cd7eaf2a54a8ea05e4daeb4787df8c
David Seeler <david.seeler@gmail.com>
2023-07-01 15:29:33 -0400

    Initial Commit

---
+++ visionos-ordering/visionos_orderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionos_orderingApp.swift
+//  visionos-ordering
+//
+//  Created by David Seeler on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct visionos_orderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
